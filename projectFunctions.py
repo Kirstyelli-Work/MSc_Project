f297165d1882dbfe62efd9918787e14e44c85372
Kirsten Elliott <kirstyelli@gmail.com>
2026-07-05 19:09:45 +0200

    Initial commit

---
+++ projectFunctions.py
@@ -0,0 +1,214 @@
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.mixture import BayesianGaussianMixture
+import pandas as pd
+import numpy as np
+import os
+import shutil
+import random
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def myMBKM(features, n_clusters=20, max_iter=10, n_init="auto"):
+    # Perform fit
+    my_kmeans = MiniBatchKMeans(n_clusters=n_clusters, max_iter=max_iter, n_init=n_init).fit(features)
+    # Predict Labels, Centers and Distances
+    kmeans_labels = my_kmeans.predict(features)
+    kmeans_centers = my_kmeans.cluster_centers_
+    kmeans_dist = my_kmeans.transform(features)
+    # Create Distance Column Names
+    dist_cols = []
+    for i in range(kmeans_dist.shape[1]):
+        dist_cols.append(f'Distance_{i}')
+    # Saving cluster distance information
+    clusters = pd.DataFrame(index = features.index.copy())
+    clusters['Cluster'] = kmeans_labels
+    clusters['Center'] = [list(kmeans_centers[i]) for i in kmeans_labels]
+    clusters[dist_cols] = kmeans_dist
+
+    return clusters
+
+def myBGMM(features, n_components=20, weight_concentration_prior=0.5, n_init=10, max_iter=1000):
+    # Perform fit
+    my_bgmm = BayesianGaussianMixture(n_components=n_components, weight_concentration_prior=weight_concentration_prior, n_init=n_init, max_iter=max_iter).fit(features)
+    # Predict Labels, Centers and Probabilities
+    bgmm_labels = my_bgmm.predict(features)
+    bgmm_centers = my_bgmm.means_
+    bgmm_prob = my_bgmm.predict_proba(features)
+    # Create Probability Column Names
+    prob_cols = []
+    for i in range(bgmm_prob.shape[1]):
+        prob_cols.append(f'Prob_{i}')
+    # Saving cluster probability information
+    clusters = pd.DataFrame(index = features.index.copy())
+    clusters['Cluster'] = bgmm_labels
+    clusters['Center'] = [list(bgmm_centers[i]) for i in bgmm_labels]
+    clusters[prob_cols] = bgmm_prob
+
+    return clusters
+
+def create_gz_evaluation_set(
+        input_df, question_level=1, min_votes=0, max_votes=None, half_votes_min=True, min_prob=0, max_prob=1, min_sources=100):
+    """
+    Create a filtered evaluation set from a DataFrame of classification responses from Galaxy Zoo.
+
+    Parameters
+    ----------
+    input_df : pandas.DataFrame
+        DataFrame containing classification responses, where each row corresponds to an object
+        and columns include vote counts, probabilities, or other relevant features.
+
+    question_level : int, default=1
+        The depth level of the question in a decision tree or hierarchy to evaluate. For example,
+        `question_level=1` may correspond to the top-level question.
+    
+    min_votes : int or None, optional
+        Minimum number of total votes for first question required for a sample to be included. If `None`, no
+        threshold is applied based on vote count. Recommended minimum from Walmsley et al is 34
+
+    max_votes : int or None, optional
+        Maximum number of total votes for first question  required for a sample to be included. If `None`, no
+        threshold is applied based on vote count. This can be used to define an "uncertain" sample.
+        
+    half_votes_min : bool, default=True
+        If `True`, ensures that at least half of the possible votes were cast for the level of
+        question under consideration. Recommended to always be set to true.
+
+    min_prob : float or None, optional
+        Minimum probability threshold for including a classification. If `None`, no filtering 
+        based on probability is applied.
+
+    max_prob : float or None, optional
+        Maximum probability threshold for including a classification. If `None`, no filtering 
+        based on probability is applied. This can be used to define an "uncertain" sample.
+
+    min_sources : int, optional
+        Minimum number of sources before a specific combination of questions can be considered a class.
+
+    Returns
+    -------
+    evaluation_set : pandas.DataFrame
+        A filtered DataFrame including only the samples that meet the specified criteria.
+
+    classes_summary : pandas.DatFrame
+        A summary of each target with the unique set of answers and number of sources in that class.
+    """
+    # questions = [
+    #     'smooth-or-featured','disk-edge-on', 'how-rounded','edge-on-bulge', 'bar', 'has-spiral-arms', 'merging','bulge-size', 'spiral-winding', 'spiral-arm-count']
+
+    levels_dict = { 
+                1:['smooth-or-featured'],
+                2:['merging'],
+                3:['disk-edge-on', 'how-rounded'],
+                4:['edge-on-bulge','has-spiral-arms', 'bar', 'bulge-size'], 
+                5:['spiral-winding', 'spiral-arm-count']
+              }
+    ignore_list = ['total-votes', 'fraction', 'debiased']
+    questions   = []
+
+    for lvl in levels_dict.keys():
+        if lvl <= question_level:
+            questions += levels_dict[lvl]
+    
+    # Step 0: Filter rows based on total votes for the first question
+    msk = input_df['smooth-or-featured_total-votes'] >= min_votes
+    if max_votes is not None:
+        msk = msk*(input_df['smooth-or-featured_total-votes'] < max_votes)
+    df = input_df.loc[msk]
+
+    answers_df = pd.DataFrame(index=df.index)
+
+    for q in questions:
+        # Step 1: Identify answer columns
+        answer_cols = []
+        for c in df.columns:
+            # Check if the query string appears in the column name, if so, add it to answer_cols
+            if q in c:
+                suffix = c.split('_')[-1] # Get the last part of the column name
+                if suffix not in ignore_list:
+                    answer_cols.append(c)
+        
+        # Step 2: Get vote values and best answer per row
+        vote_vals = df[answer_cols].values  # shape (n_rows, n_answers)
+        best_idx = np.argmax(vote_vals, axis=1)  # shape (n_rows,)
+        best_answers = np.array(answer_cols)[best_idx]  # shape (n_rows,)
+        best_votes = vote_vals[np.arange(len(df)), best_idx] # shape (n_rows,)
+        total_votes = vote_vals.sum(axis=1)
+        
+        # Step 3: Get corresponding fraction values using NumPy indexing        
+        best_fractions = np.zeros(len(df))
+        msk = total_votes != 0
+        best_fractions[msk] = best_votes[msk]/total_votes[msk]
+    
+        # Step 4: Apply Probability Filter
+        prob_cut_mask = (best_fractions < min_prob) | (best_fractions > max_prob)
+        best_answers[prob_cut_mask] = 'N/A'
+
+        # Step 5: Apply Half Votes Filter
+        if half_votes_min is True:
+            half_vote_cut = total_votes < 0.5 * df['smooth-or-featured_total-votes'] #Why do we trust this one though?
+            best_answers[half_vote_cut] = 'N/A'
+
+        # Step 6: Store best answers in the answers_df
+        answers_df[q] = best_answers
+        
+    # Step 7: Only keep rows where each level has at least one valid answer (tag)
+    for lvl in range(1, question_level+1): 
+        answers_df = answers_df[~np.all(answers_df[levels_dict[lvl]]=='N/A', axis=1)]
+
+    # Step 8: Assign target labels to each class and create classes summary dataframe
+    counts = answers_df.value_counts() # Get counts of each unique combination of answers
+    counts = counts[counts>min_sources]
+    top_rows = counts.reset_index() # Convert to DataFrame and reset index
+    top_rows['target'] = range(len(top_rows)) # Assign target labels as integers starting from 0
+    classes_summary = pd.DataFrame(top_rows)
+    classes_summary.set_index('target', inplace=True)
+    
+    # Step 9: Merge df_reset (modified answers_df) with top rows to assign target labels to each row in the original answers_df
+    # (if the combination of answers is in the top rows, it gets the target label, otherwise it is dropped)
+    df_reset = answers_df.reset_index() # Adding column of iauname from index of answers_df
+    top_rows = top_rows.drop(columns=['count'])  # Drop the count column
+    df_top = df_reset.merge(top_rows, on=list(answers_df.columns), how='inner')
+    df_top = df_top.set_index('iauname') # Restore original index
+    
+    # Step 10: Adding complexity column showing no. of tags for each target class
+    classes_summary['complexity'] = (classes_summary[classes_summary.columns[:-1]]!='N/A').sum(axis=1)
+    
+    return df_top, classes_summary
+
+def show_random_images(folder, classification, data_root_dir):
+
+    files = [f for f in os.listdir(folder) if f.endswith('.png')]
+    random_images = random.sample(files, 5)
+
+    fig, axes = plt.subplots(1, 5, figsize=(10,2))
+
+    for ax, img_name in zip(axes.flatten(), random_images):
+
+        img_path = os.path.join(folder, img_name)
+        img = Image.open(img_path)
+        label = os.path.splitext(img_name)[0]
+
+        ax.imshow(img)
+        ax.text(0.98, 0.98, label,
+                transform=ax.transAxes,
+                ha='right', va='top',
+                color='white', fontsize=9)
+        ax.axis("off")
+
+    plt.tight_layout()
+    plt.savefig(f'{classification}_random_images.png')
+
+def saving_images(classification, class_name,img_dir, data_root_dir):
+    
+        filenames_to_find = set(classification.astype(str) + '.png')
+        count = 0
+
+        for root, dirs, files in os.walk(img_dir):
+            for file in files:
+                if file in filenames_to_find:
+                    src_path = os.path.join(root, file)
+                    dest_path = os.path.join(data_root_dir, f'Galaxy_Images/{class_name}', file)
+                    shutil.copy2(src_path, dest_path)
+                    count += 1
+        print(f'{class_name}: {count}')
+        show_random_images(os.path.join(data_root_dir, f'Galaxy_Images/{class_name}'), class_name, data_root_dir)
